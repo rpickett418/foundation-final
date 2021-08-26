1d29cd6b2b63164b7f37c995615632d97d19b5ca
rpickett418 <86981398+rpickett418@users.noreply.github.com>
2021-08-26 02:33:11 -0600

    final-pro

--- client/main.js
+++ client/main.js
@@ -1,13 +1,201 @@
-let itemCount = document.querySelector("#shopcart-count");
 
-let count = 0;
 
-function addItemsCart() {
-    count = count +1
-    if(count === 1) {
-        itemCountText.textContent = '1 item';
-    }else {
-        itemCountText.textContent = count + "items"
+let carts = document.querySelectorAll('.add-cart');
+const bronBtn = document.getElementById('bronBtn')
+const stephBtn = document.getElementById('stephBtn')
+const donBtn = document.getElementById('donBtn')
+const kdBtn = document.getElementById('kdBtn')
+const giannisBtn = document.getElementById('giannisBtn')
+const dieselBtn = document.getElementById('dieselBtn')
+const signUpForm = document.querySelector(".email-sign-up");
+const emailInput = document.querySelector("input");
+const signUpbtn = document.querySelector("#sign-up");
+const footer = document.querySelector("footer");
+
+document.getElementById('shopcart-count')
+// Shopping Cart Stuff
+let products = [
+    {
+        name: 'Utah Jazz',
+        tag: 'utahjazz',
+        price: 34,
+        inCart: 0,
+
+    },
+    {
+        name: 'Milwaukee Bucks',
+        tag: 'milwaukeebucks',
+        price: 45,
+        inCart: 0,
+
+    },
+    {
+        name: 'Houston Rockets',
+        tag: 'houstonrockets',
+        price: 35,
+        inCart: 0,
+
+    },
+    {
+        name: 'New York Knicks',
+        tag: 'newyorkknicks',
+        price: 35,
+        inCart: 0,
+
+    },
+    {
+        name: 'Denver Nuggets',
+        tag: 'denvernuggets',
+        price: 35,
+        inCart: 0,
+
+    }
+]
+for (let i = 0; i < carts.length; i++) {
+    carts[i].addEventListener('click', () => {
+        // each product has an index
+        cartNumbers(products[i]);
+        totalCost(products[i])
+    })
+}   
+// First run
+function onLoadCartNumbers(){
+    let productNumbers = localStorage.getItem('cartNumbers');
+    
+    if(productNumbers){
+        document.querySelector('.cart span').textContent = productNumbers;
     }
 }
+function cartNumbers(product) {
+    // console.log('This is ', product);
+    let productNumbers = localStorage.getItem('cartNumbers');
 
+    // converting strings into numbers 
+    productNumbers = parseInt(productNumbers);
+
+    if(productNumbers) {
+        localStorage.setItem('cartNumbers', productNumbers + 1);
+        document.querySelector('.cart span').textContent = productNumbers + 1;
+    } else {
+       localStorage.setItem('cartNumbers',  1);
+       document.querySelector('.cart span').textContent = 1;
+    }
+    setItems(product)
+    
+}
+function setItems(product) {
+    console.log('inside of setItems function');
+    console.log('My product is', product);
+
+   let cartItems = localStorage.getItem('productsInCart');
+    cartItems = JSON.parse(cartItems);
+    
+    if (cartItems != null) {
+        if ( cartItems[product.tag] == undefined) {
+            cartItems = {
+                ...cartItems,
+                [product.tag]:product
+            }
+        }
+        cartItems[product.tag].inCart += 1;
+    } else {
+        product.inCart= 1;
+        cartItems = {
+            [product.tag]:product
+        }
+    }
+
+    localStorage.setItem('productsInCart', JSON.stringify (cartItems));
+}
+    
+
+function totalCost(product){
+    let cartCost = localStorage.getItem('totalCost');
+        // console.log('my product price is', cartCost);
+       console.log(typeof cartCost);
+        cartCost = parseInt(cartCost); 
+        localStorage.setItem('totalCost', product.price);
+        
+        if(cartCost != null) {
+            localStorage.setItem('totalCost', cartCost + product.price);
+        } else {
+            localStorage.setItem('totalCost', product.price);
+        }
+
+}
+// run on first time
+function displayCart() {
+    let cartItems = localStorage.getItem('productsInCart');
+    // converting JSON into JAVASCRIPT
+    cartItems = JSON.parse(cartItems)
+    let productContainer = document.querySelector('products-container')
+        
+    // console.log(cartItems)
+        if (cartItems && productContainer) {
+           productContainer.innerHTML = ''; 
+           Object.values(cartItems).map(item => {
+            productContainer.innerHTML += `
+            <div class="product">
+                <ion-icon name="close-circle-outline"></ion-icon>
+                    <div class="image">
+                        <span>${item.name}M/span>
+            </div>
+            `
+           });
+        }
+}
+onLoadCartNumbers();
+displayCart()
+
+// When the user clicks on div, open the popup
+function madFacts() {
+    let popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+  }
+
+// create a function for axios in frontend 
+
+function playersProfile(player) {
+    axios.get(`/api/v1/players/${player}`)
+    .then(res => {
+        // console.log(res.data.data)
+        // console.log(res.data.data.first_name)
+        console.log(res.data)
+        
+    }) 
+    .catch(err => console.log(err))
+}
+
+function emailSubmitHandler() {
+    const confirmationMsg = document.createElement("p");
+    confirmationMsg.textContent = "Verify e-mail to receive free shipping on your first order." + emailInput.value; 
+    signUpForm.remove();
+    
+    footer.appendChild(confirmationMsg);
+}
+    signUpbtn.addEventListener('click', () => {
+        emailSubmitHandler()
+    })
+
+bronBtn.addEventListener('click', () => {
+    playersProfile(237)
+})
+stephBtn.addEventListener('click', () => {
+    playersProfile(115)
+})
+donBtn.addEventListener('click', () => {
+    playersProfile(322)
+})
+kdBtn.addEventListener('click', () => {
+    playersProfile(140)
+})
+giannisBtn.addEventListener('click', () => {
+    playersProfile(15)
+})
+dieselBtn.addEventListener('click', () => {
+    playersProfile(735)
+})
+signUpbtn.addEventListener('click', () => {
+    emailSubmitHandler()
+})
+// setTimeout(donBtn(alert), 5000000 );
---
+++ server/index.js
@@ -0,0 +1,69 @@
+const express = require('express')
+const cors = require('cors')
+const app = express();
+const ctrl = require('./controller')
+const axios = require('axios')
+const path = require('path')
+
+app.use(express.json());
+app.use(cors());
+app.use(express.static('client'))
+
+let basketballItems = null;
+    
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/index.html'))
+})
+
+app.get(`/api/v1/players/:player`, (req, res) => {
+    let playerId = req.params.player
+    // axios.get(`https://www.balldontlie.io/api/v1/players/${playerId}`)
+    axios.get(`https://www.balldontlie.io/api/v1/players/${playerId}`)
+    .then(
+        resTwo => res.status(200).send(resTwo.data))
+        
+    .catch(err => res.sendStatus(err))
+})
+
+app.get("/api/donBtn", (req,res) => {
+    const spiderMitch = [
+      "Donovan Mitchell averaged 26.4 points during the 2020-21 season, helping his team break historic franchise records."
+    ];
+    
+    res.status(200).send(spiderMitch)
+});
+
+app.get("/api/bronBtn", (req,res) => {
+    const kingJames = [
+      "King James averaged 25 point per game during the 2020-21 season."
+    ];
+    res.status(200).send(kingJames)
+});
+
+app.get("/api/stephBtn", (req,res) => {
+    const babyFaceAssassin = [
+      "The Splash brother himself averaged 32 points 2020-21 season, with a tough fight to try and carry his team in the play-in games."
+    ];
+    res.status(200).send(babyFaceAssassin)
+});
+app.get("/api/kdBtn", (req,res) => {
+    const easyMoneySniper = [
+      "KD @easymoneysniper who is considered the greatest player of the NBA averaged 26.9 points in the 2020-21 season."
+    ];
+    res.status(200).send(easyMoneySniper)
+});
+app.get("/api/giannisBtn", (req,res) => {
+    const theGreekFreak = [
+      "Giannis AKA The Greek Freak AKA the world wide NBA MVP Champion averaged 35.2 points in the 2020-21 NBA championship series."
+    ];
+    res.status(200).send(theGreekFreak)
+});
+app.get("/api/dieselBtn", (req,res) => {
+    const mayorMcShaq = [
+      "Shaq aka The diesel average 29.7 points and 13.6 rebound in the 1999-2000 season."
+    ];
+    res.status(200).send(mayorMcShaq)
+});
+
+
+app.listen(4044, () => console.log("Hurry Up"));
